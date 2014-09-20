9bc4368e274dd6ed0e07c7cb8d686fbf3b88a6d2
hlipro <hlipro@bu.edu>
2014-09-20 11:28:01 -0400

    add

--- mycompany/hello.py
+++ mycompany/hello.py
@@ -6,6 +6,23 @@
 __author__ = 'Hao Li'
 
 import sys
+# use alias to import module
+try:
+	import cStringIO as StringIO
+except ImportError:
+	import StringIO
+try:
+	import json # python >=2.6
+except ImportError:
+	import simplejson as json # python < 2.6
+
+# private functions/variables are defined by adding _ or __ ahead of name
+# private functions are only used inside module to pack sourse codes
+# public functions are open, and could call private functions
+def _private_1():
+	print 'Hello, world!'
+def _private_2(args):
+	print 'Hello, %s!'%args[1]
 
 def test():
 	args = sys.argv
@@ -13,9 +30,9 @@ def test():
 	# argv[0] = the name of called module
 	# E.g., python hello.py : argv[0] = hello.py
 	if len(args) == 1:
-		print 'Hello, world!'
+		_private_1()
 	elif len(args) == 2:
-		print 'Hello, %s!'%args[1]
+		_private_2(args)
 	else:
 		print 'Too many arguments!'
 
