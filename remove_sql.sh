91e0628776dad583f207dc2a18e7cca7278639ee
hlipro <hlipro@bu.edu>
2014-09-21 04:41:04 -0400

    add mac cheat sheet

---
+++ remove_sql.sh
@@ -0,0 +1,9 @@
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/MySQL*
+rm -rf ~/Library/PreferencePanes/MySQL*
+sudo rm -rf /Library/Receipts/mysql*
+sudo rm -rf /Library/Receipts/MySQL*
+sudo rm -rf /var/db/receipts/com.mysql.*
+sudo vim /etc/hostconfig
